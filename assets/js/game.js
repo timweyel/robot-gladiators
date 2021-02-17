388f47dfd071438c731285f3234c3274e4a4427f
Tim Weyel <tweyel@gmail.com>
2021-02-16 17:55:10 -0800

    Enhancement: Randomize fight order in the fight() function for each new enemy-robot round

--- assets/js/game.js
+++ assets/js/game.js
@@ -85,6 +85,15 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+
+  //keep track of who goes first
+  var isPlayerTurn = true;
+
+  //randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
   // repeat and execute as long as the enemy-robot is alive 
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or skip using fightOrSkip function
@@ -153,6 +162,8 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  // switch turn order for next round
+  isPlayerTurn = isPlayerTurn;
 };
 
 // go to shop between battles function
